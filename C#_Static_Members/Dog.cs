d744881208d382367ed6efe8ddefc86486c5fa22
TaeYoun(Jack) Kwon <114732820+kweont0211@users.noreply.github.com>
2022-10-13 14:25:27 +0900

    C#_Static_Members

---
+++ C#_Static_Members/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Example_01
+{
+    class Dog
+    {
+        //Data Members (Fields)
+        public static string BarkMessage;
+        public static int BarkCount;
+
+        //Methods
+        public static void Bark()
+        {
+            for (int count = 0; count < BarkCount; count++)
+            {
+                Console.WriteLine(BarkMessage);
+            }
+        }
+    }
+}
---
+++ C#_Static_Members/Program.cs
@@ -0,0 +1,21 @@
+﻿//This tells the compiler/linker to look for Classes in the System namespace,
+//which is in the System.dll code library.
+using System; 
+
+namespace Example_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog.BarkCount = 5;
+            Dog.BarkMessage = "Ruff!";
+
+            Dog.Bark();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
